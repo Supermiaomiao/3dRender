09ad5820d7fdb066359083701207ccea7a09fbe8
李彦莹 <370644245@qq.com>
2020-05-28 21:59:49 +0800

    draw illidan

--- renderer/canvas.js
+++ renderer/canvas.js
@@ -31,7 +31,6 @@ class GuaCanvas extends GuaObject {
     }
     render() {
         // 执行这个函数后, 才会实际地把图像画出来
-        // ES6 新语法, 取出想要的属性并赋值给变量, 不懂自己搜「ES6 新语法」
         let {pixels, context} = this
         context.putImageData(pixels, 0, 0)
     }
@@ -372,8 +371,6 @@ class GuaCanvas extends GuaObject {
         }
     }
     __debug_draw_demo() {
-        // 这是一个 demo 函数, 用来给你看看如何设置像素
-        // ES6 新语法, 取出想要的属性并赋值给变量, 不懂自己搜「ES6 新语法」
         let {context, pixels} = this
         // 获取像素数据, data 是一个数组
         let data = pixels.data
